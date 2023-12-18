2755f873a7fb7e3d9222a8475f03831f800517ba
ddr4869 <82074897+ddr4869@users.noreply.github.com>
2023-12-18 14:12:59 +0900

    [level 2] Title: 괄호 변환, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+def isRight(formula):
+    cnt=0
+    for i in formula:
+        if i=='(': cnt+=1
+        else: cnt-=1
+        if cnt<0: return False
+    
+    if cnt==0: return True
+    else: return False
+
+def seperateUV(formula):
+    if not formula:
+        return None, None
+    u,v,cnt ='','',0
+    tmp=deque(formula)
+    print(u)
+    for i in formula:
+        if i=='(': cnt+=1
+        else:  cnt-=1    
+        u+=str(tmp.popleft())
+        #u+=val
+        if cnt==0:
+            v=''.join(tmp)
+            break
+    print("u:",u)
+    print("v:",v)
+    return u,v
+
+def process(w):
+    # 1
+    if not w:
+        return ''
+    # 2 u,v로 분리
+    u,v=seperateUV(w)
+    if isRight(u):
+        return u+process(v) # 3-1
+    else:
+        answer='('+process(v)+')' #4-1~3
+        u=u[1:-1]      
+        return answer+''.join([')' if i=='(' else '(' for i in u])
+
+def solution(p):
+    if isRight(p):
+        return p
+    answer=process(p)
+    return answer
