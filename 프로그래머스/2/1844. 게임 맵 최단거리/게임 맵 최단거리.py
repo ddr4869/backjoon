aefd05c86ce523319786f7b5a6fc882be44fa870
ddr4869 <82074897+ddr4869@users.noreply.github.com>
2023-12-28 14:54:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.60 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,25 +2,22 @@ from collections import deque
 def solution(maps):
     dq=deque()
     dq.append((0,0,1))
-    answer = 0
     N=len(maps)
     M=len(maps[0])
     dp=[[0 for _ in range(M)] for _ in range(N)]
-    ans=0
     while dq:
         v=dq.popleft()
         if dp[v[0]][v[1]]==0:
             dp[v[0]][v[1]]=v[2]
         else:
             continue
-        if v[0]+1<N and maps[v[0]+1][v[1]] and not dp[v[0]+1][v[1]]:
+        if v[0]+1<N and maps[v[0]+1][v[1]]:
             dq.append((v[0]+1,v[1],v[2]+1))
-        if v[0]-1>=0 and maps[v[0]-1][v[1]] and not dp[v[0]-1][v[1]]:
+        if v[0]-1>=0 and maps[v[0]-1][v[1]]:
             dq.append((v[0]-1,v[1],v[2]+1))
-        if v[1]+1<M and maps[v[0]][v[1]+1] and not dp[v[0]][v[1]+1]:
+        if v[1]+1<M and maps[v[0]][v[1]+1]:
             dq.append((v[0],v[1]+1,v[2]+1))
-        if v[1]-1>=0 and maps[v[0]][v[1]-1] and not dp[v[0]][v[1]-1]:
+        if v[1]-1>=0 and maps[v[0]][v[1]-1]:
             dq.append((v[0],v[1]-1,v[2]+1))
 
-            
     return -1 if dp[N-1][M-1]==0 else dp[N-1][M-1] 
