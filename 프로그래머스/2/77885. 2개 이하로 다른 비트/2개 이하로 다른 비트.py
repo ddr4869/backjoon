d81a4ca1a16ea532479a303eb4b7f8076fabf4a3
ddr4869 <82074897+ddr4869@users.noreply.github.com>
2023-12-28 14:23:16 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 831.30 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,3 +1,24 @@
+import math
+
+def calc_result(binary):
+    binary_len=len(binary)
+    idx=binary_len-1
+    while(idx>=0 and binary[idx]=='1'):
+        idx-=1
+    
+    binary=list(binary)
+    binary[idx]='1'
+    if idx+1<binary_len:
+        binary[idx+1]='0'
+    binary=''.join(binary)
+    return int(binary,2)
+
 def solution(numbers):
     answer = []
+    for number in numbers:
+        if number==1:
+            answer.append(2)
+            continue
+        binary='0'+bin(number)[2:]
+        answer.append(calc_result(binary))
     return answer
