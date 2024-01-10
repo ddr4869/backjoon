4aa4e3c2f60818a4f1143154949ca1333fbbf879
ddr4869 <82074897+ddr4869@users.noreply.github.com>
2024-01-10 10:12:03 +0900

    [level 2] Title: 소수 찾기, Time: 0.62 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,3 +1,26 @@
+from itertools import permutations
+from collections import defaultdict
+import math
+
+def isPirme(num):
+    if num<=1: return False
+    end=int(math.sqrt(num))+1
+    for i in range(2,end):
+        if (num%i)==0:
+            return False
+    return True
+
 def solution(numbers):
     answer = 0
+    idx=1
+    flag=defaultdict(int)
+    while(idx<=len(numbers)):
+        perm=permutations(numbers, idx)
+        for i in perm:
+            num=int(''.join(i))
+            if flag[num]==0 and isPirme(num): 
+                answer+=1
+                flag[num]=1
+        idx+=1
+
     return answer
