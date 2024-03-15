598d76ba4696c7c9a0687749c27a442c32dfcb1f
tom <82074897+ddr4869@users.noreply.github.com>
2024-03-15 09:31:51 +0900

    [level 3] Title: 숫자 게임, Time: 47.52 ms, Memory: 18.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    A.sort(); B.sort()
+    answer = 0
+    while(A and B):
+        if B[-1]>A[-1]:
+            answer+=1; B.pop(); A.pop()
+        else:
+            A.pop()
+    return answer
