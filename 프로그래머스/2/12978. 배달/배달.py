6e70b087a3bad8e43e8f8cfef9e9eaf7987e1729
tom <82074897+ddr4869@users.noreply.github.com>
2024-03-24 01:05:53 +0900

    [level 2] Title: 배달, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,18 +1,25 @@
-# 플로이드
+# 다익스트라
+from collections import defaultdict
+import heapq
 def solution(N, road, K):
-    dp=[[10e8 for _ in range(N+1)] for _ in range(N+1)]
+    dp=[10e12 for _ in range(N+1)]
+    dict=defaultdict(list)
     for start,arrive,cost in road:
-        dp[start][arrive]=min(cost,dp[start][arrive])
-        dp[arrive][start]=min(cost,dp[arrive][start])
-    for i in range(1,N+1):
-        dp[i][i]=0
-    
-    for k in range(1,N+1):
-        for i in range(1,N+1):
-            for j in range(1,N+1):
-                if dp[i][j]>dp[i][k]+dp[k][j]:
-                    dp[i][j]=dp[i][k]+dp[k][j]
+        dict[start].append((arrive,cost))
+        dict[arrive].append((start,cost))
+    stack=[]
+    heapq.heappush(stack,(1,0)) # point, cost
+    dp[1]=0
+    while(stack):
+        current,cost=heapq.heappop(stack)
+        if dp[current] < cost: 
+            continue
+        next_list=dict[current]
+        for _next,next_cost in next_list:
+            if dp[_next]>cost+next_cost:
+                dp[_next]=cost+next_cost
+                heapq.heappush(stack,(_next,cost+next_cost))
     answer=0
     for i in range(1,N+1):
-        if dp[1][i]<=K: answer+=1
+        if dp[i]<=K: answer+=1
     return answer
