1e388054d03db7a83004ca9c8193c66d1bc26c87
tom <82074897+ddr4869@users.noreply.github.com>
2024-03-24 00:36:16 +0900

    [level 2] Title: 배달, Time: 22.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,19 +1,18 @@
-
-def solve(N, K, dist, cnt, here, min_dist):
-    if cnt>K or cnt>min_dist[here]: return
-    min_dist[here]=cnt
-    for i in range(1,N+1):
-        if dist[here][i]!=10001:
-            solve(N, K, dist, cnt+dist[here][i], i, min_dist)
-
+# 플로이드
 def solution(N, road, K):
-    answer=0
-    dist=[[10001 for _ in range(N+1)] for _ in range(N+1)]
-    min_dist=[500001 for _ in range(N+1)]
-    for info in road:
-        dist[info[0]][info[1]]=min(info[2], dist[info[0]][info[1]])
-        dist[info[1]][info[0]]=min(info[2], dist[info[1]][info[0]])
-    solve(N,K,dist,0,1,min_dist)
+    dp=[[10e8 for _ in range(N+1)] for _ in range(N+1)]
+    for start,arrive,cost in road:
+        dp[start][arrive]=min(cost,dp[start][arrive])
+        dp[arrive][start]=min(cost,dp[arrive][start])
     for i in range(1,N+1):
-        if min_dist[i]<=K: answer+=1
+        dp[i][i]=0
+    
+    for k in range(1,N+1):
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                if dp[i][j]>dp[i][k]+dp[k][j]:
+                    dp[i][j]=dp[i][k]+dp[k][j]
+    answer=0
+    for i in range(1,N+1):
+        if dp[1][i]<=K: answer+=1
     return answer
