3800af6d80c79496986fc980046576fa60e7a05a
tom <82074897+ddr4869@users.noreply.github.com>
2024-04-03 11:06:46 +0900

    [Gold II] Title: 트리의 지름, Time: 388 ms, Memory: 157624 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -14,23 +14,19 @@ answer=0
 visited=[0 for _ in range(n+1)]
 def dfs(here, visited, cost):
     global answer
+    answers=[]
     if visited[here]!=0: return 0
     visited[here]=1
-    answers=[]
     for next_cost, _next in link[here]:
         if visited[_next]==0:
             ans=dfs(_next, visited, cost)+next_cost
-            if len(answers)==0: answers.append(ans)
-            elif len(answers)==1:
-                if answers[0]<ans: answers=[ans,answers[0]]
-                else: answers.append(ans)
-            else:
-                if answers[0]<ans: answers=[ans,answers[0]]
-                elif answers[0]>=ans>answers[1]: answers[1]=ans
-    if len(answers)==2 and sum(answers)>answer: answer=sum(answers)
-    ret=answers[0]+cost if answers else cost        
-    if ret>answer: answer=ret
-    return ret
+            answers.append(ans) 
+    if answers: 
+        answers.sort(reverse=True)
+        if len(answers)>=2: 
+            answer=max(answer,answers[0]+answers[1])
+        else: answer=max(answer,answers[0])
+    return answers[0]+cost if answers else cost        
 
 dfs(1,visited,0)
 print(answer)
