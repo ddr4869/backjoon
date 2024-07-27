1d2b67163b1c1a183e39c04feb4755eb4fbb0b35
tom <82074897+ddr4869@users.noreply.github.com>
2024-07-27 23:44:33 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 21.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -7,7 +7,7 @@ def solution(board):
     if board[0][1]!=1:
         dq.append((0,1,'x',100)); visited[0][1][0]=100
     if board[1][0]!=1:
-        dq.append((1,0,'y',100)); visited[1][0][1]=100 #(y,x,direction,cost)
+        dq.append((1,0,'y',100)); visited[1][0][1]=100 
         
     dy=[1,-1,0,0]
     dx=[0,0,1,-1]
@@ -19,13 +19,13 @@ def solution(board):
         for i in range(4):
             ny,nx=y+dy[i],x+dx[i]
             if 0<=ny<N and 0<=nx<M and board[ny][nx]!=1:
-                if direction=='x' and dx[i]==0 and cost+600<visited[ny][nx][1]: # y로 전환
+                if direction=='x' and dx[i]==0 and cost+600<visited[ny][nx][1]: 
                     visited[ny][nx][1]=cost+600
                     dq.append((ny,nx,'y',cost+600))
                 elif direction=='x' and dx[i]!=0 and cost+100<visited[ny][nx][0]:
                     visited[ny][nx][0]=cost+100
                     dq.append((ny,nx,'x',cost+100))
-                elif direction=='y' and dy[i]==0 and cost+600<visited[ny][nx][0]: # x로 전환
+                elif direction=='y' and dy[i]==0 and cost+600<visited[ny][nx][0]:
                     visited[ny][nx][0]=cost+600
                     dq.append((ny,nx,'x',cost+600))
                 elif direction=='y' and dy[i]!=0 and cost+100<visited[ny][nx][1]:
